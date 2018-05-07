b2c2c0006829ec7a4ca478e6048e804cb42423c8
dbachinin <dbachinin@ya.ru>
2018-05-07 13:30:20 +0300

    modified index

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,22 @@
 Rails.application.routes.draw do
+  get 'people/index'
+
+  get 'people/show'
+
+  get 'people/edit'
+
+  get 'people/update'
+
+  get 'people/destroy'
+
     devise_for :users, :path => 'users'
     resources :users do
-    	resources :projects
+    	resources :projects, param: :uid, shallow: true #do
+      #   resources :tasks, shallow: true
+      # end
   	end
   resources :tasks
-
+  # get 'users/:id/projects/:id', to: 'projects#show', as: 'project'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "projects#index"
   get "/edit_comments" => 'tasks#edit_coments'
