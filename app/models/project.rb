18a94950c7fac011d334527a2c64c7ccd4a5bb61
dbachinin <dbachinin@ya.ru>
2018-05-16 01:07:33 +0300

    before push

--- app/models/project.rb
+++ app/models/project.rb
@@ -6,18 +6,19 @@ class Project
   field :description, type: String
   field :uid, type: String
   field :user_id, type: String
-  
+  field :back_uid, type: String
   belongs_to :user
   has_many :project, autosave: true
   accepts_nested_attributes_for :project, allow_destroy: true
   after_initialize :gen_uid, if: :new_record?
-  before_save :save_uid, unles: :new_record?
+  before_save :save_uid, unless: :new_record?
   def gen_uid
     self.uid = SecureRandom.uuid[6..12]
+    self.back_uid = self.uid
   end
 
   def save_uid
-    self.uid = File.read('tmp/uid')
+    self.uid = back_uid
   end
 
 end
